f11827a081c077a341a4d9eadfb750f6f57390d4
tentao7 <40426588+tentao7@users.noreply.github.com>
2020-06-10 10:58:42 +0900

    Add files via upload
    Every day 1 at U-Tube

---
+++ Learn+Python_Full+course+for+beginners-Copy1.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+print("    /l")
+print("   / l")
+print("  /  l")
+print(" /___l")
+
+
+# In[3]:
+
+
+
+print("There once a man named George,")
+print("he was 70 years old.")
+print(" He reallly like the name George,")
+print("but did't like being 70.")
+
+
+# In[4]:
+
+
+character_name = "Tom" # string 
+character_age  = 50   #integer
+is_Male = False      #boolean
+print("There once a man named "+ character_name+",")
+print("he was %i years old.", character_age)
+character_name = " Mike"
+print(" He reallly like the name"+ character_name+",")
+print("but did't like being %i", character_age)
+
+
+# In[5]:
+
+
+phrase = "Giraffe Academy"
+
+print("Giraffe\"Academy") # print " 
+     
+
+
+# In[6]:
+
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+
+
+# In[11]:
+
+
+print(phrase.lower())
+#print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+
+# In[12]:
+
+
+print(len(phrase))
+
+
+# In[14]:
+
+
+print(phrase[0])
+
